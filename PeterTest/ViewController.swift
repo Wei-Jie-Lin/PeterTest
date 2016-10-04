2a0863ba8915baf96c19ea47d55c2c9231c09c40
家人一暐潔 <Wei-Jie@linweijiede-macbook-pro.local>
2016-10-04 19:57:02 +0800

    加入	4個 button

--- PeterTest/ViewController.swift
+++ PeterTest/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 哈囉 你好。
         // Do any additional setup after loading the view, typically from a nib.
     }
 
